bf121768cf15cabcf73fcc4720e965d2aaee6eba
root <root@ip-172-31-32-184.ap-south-1.compute.internal>
2023-04-21 02:32:47 +0000

    mvn project creation

---
+++ src/main/java/com/reddimasu/App.java
@@ -0,0 +1,13 @@
+package com.reddimasu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
